b27949cefdb8f3d18b228dcc40fac6cd277469d3
LunarCodeZ <erik.atatawiyu@gmail.com>
2026-04-26 05:12:41 +0700

    09-09-2026: Tugas - Jadwal Pelajaran

---
+++ semester-1/tugas-pak-isa/kelas/09-09-2026/JadwalPelajaran/Senin.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JadwalPelajaran
+{
+    internal class Senin
+    {
+        public void sesi1()
+        {
+            Console.WriteLine(">>> Pelajaran Hari Senin: Sesi 1 <<<");
+            Console.WriteLine("-----------------------------------------");
+            Console.WriteLine("Jam ke-  | Waktu         | Mata Pelajaran");
+            Console.WriteLine("-----------------------------------------");
+            Console.WriteLine("0        | 07.00 - 07.35 | Upacara");
+            Console.WriteLine("1        | 07.35 - 08.10 | B. Jepang");
+            Console.WriteLine("2        | 08.10 - 08.45 | B. Jepang");
+            Console.WriteLine("3        | 08.45 - 09.20 | B. Inggris");
+            Console.WriteLine("4        | 09.20 - 09.55 | B. Inggris");
+            Console.WriteLine("-        | 09.55 - 10.10 | Istirahat");
+            Console.WriteLine("-----------------------------------------");
+        }
+
+        public void sesi2()
+        {
+            Console.WriteLine(">>> Pelajaran Hari Senin: Sesi 2 <<<");
+            Console.WriteLine("-----------------------------------------");
+            Console.WriteLine("Jam ke-  | Waktu         | Mata Pelajaran");
+            Console.WriteLine("-----------------------------------------");
+            Console.WriteLine("5        | 10.10 - 10.40 | Matematika");
+            Console.WriteLine("6        | 10.40 - 11.10 | Matematika");
+            Console.WriteLine("7        | 11.10 - 11.40 | Matematika");
+            Console.WriteLine("-        | 11.40 - 12.30 | Istirahat");
+            Console.WriteLine("-----------------------------------------");
+        }
+
+        public void sesi3() {
+            Console.WriteLine(">>> Pelajaran Hari Senin: Sesi 3 <<<");
+            Console.WriteLine("-----------------------------------------");
+            Console.WriteLine("Jam ke-  | Waktu         | Mata Pelajaran");
+            Console.WriteLine("-----------------------------------------");
+            Console.WriteLine("8        | 12.30 - 13.00 | PPLG");
+            Console.WriteLine("9        | 13.00 - 13.30 | PPLG");
+            Console.WriteLine("10       | 13.30 - 14.00 | PPLG");
+            Console.WriteLine("11       | 14.00 - 14.30 | PPLG");
+            Console.WriteLine("-----------------------------------------");
+        }
+    }
+}
